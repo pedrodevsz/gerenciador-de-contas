790d2da4d7f4b46c30ee13112431f473bf9ab86b
pedrodevsz <pedrodevs25@gmail.com>
2026-04-12 13:47:22 -0300

    initial commit

---
+++ backend/src/test/java/controle_financeiro/api/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package controle_financeiro.api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
