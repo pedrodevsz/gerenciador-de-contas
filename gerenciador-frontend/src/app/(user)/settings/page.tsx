73866825a3b0e145818955305920557e3c4b1d6a
pedrodevsz <pedrodevs25@gmail.com>
2026-04-13 22:00:23 -0300

    feat: add temporary settings page

---
+++ gerenciador-frontend/src/app/(user)/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function SettingsPage() {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <p>Está página está em desenvolvimento, ficará pronta em breve</p>
+        </div>
+    )
+}
